ed04f27a9aa8bb3a239dc395c63f20d849c6cd77
MUHAMMAD BELAL <chaudharybeelal69@gmail.com>
2026-05-29 10:16:55 +0500

    Modify get_logo_base64 to include image data URI

--- assets.py
+++ assets.py
@@ -3,7 +3,9 @@ import os
 
 def get_logo_base64(filename):
     with open(filename, "rb") as f:
-        return base64.b64encode(f.read()).decode()
+        data = base64.b64encode(f.read()).decode()
+    ext = os.path.splitext(filename)[1][1:]  # png
+    return f"data:image/{ext};base64,{data}"  # ← yeh zaroori hai!
 
 IUB_LOGO = get_logo_base64("iub_logo_clean.png")
 BZU_LOGO = get_logo_base64("bzu_logo_clean.png")
